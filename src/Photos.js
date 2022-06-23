a09373ddea62186cd5496ad35d9bbc9480b3de9c
Ajvinder Singh <aju90534@gmail.com>
2022-06-23 10:29:18 +0530

    :zap: image error fixed

--- src/Photos.js
+++ src/Photos.js
@@ -1,10 +1,10 @@
 const Photos = {
-    "AtCoder": 'https://repository-images.githubusercontent.com/342857348/04ba8003-26fd-4cb9-8bc1-3cd43d786f03',
+    "AtCoder": 'https://images.velog.io/images/inwooleeme/post/f01ca242-f9e8-4773-a31f-67bd1d91481a/AtCoder.png',
     "CodeChef": 'https://static.startuptalky.com/2021/04/codechef-logo-startuptalky.jpg',
     "CodeForces": 'https://news.itmo.ru/images/news/big/917925.jpg',
     "CodeForces::Gym": 'https://news.itmo.ru/images/news/big/917925.jpg',
     "CS Academy": 'https://play-lh.googleusercontent.com/mNLWhhjA3m3fNMxW8cK9l-PgCkUkvghvnYvdob5Eze4gOeod7FdH38huer7ulzTeWV8x',
-    "HackerEarth":'https://d2908q01vomqb2.cloudfront.net/cb4e5208b4cd87268b208e49452ed6e89a68e0b8/2021/07/16/HackerEarthFeatureImage.png',
+    "HackerEarth": 'https://d2908q01vomqb2.cloudfront.net/cb4e5208b4cd87268b208e49452ed6e89a68e0b8/2021/07/16/HackerEarthFeatureImage.png',
     "HackerRank": 'https://additionalknowledge.files.wordpress.com/2017/12/hackerrank.png?w=640',
     "Kick Start": 'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20200908225219/What-is-Google-Kick-Start-Important-Dates-Eligibility-Contest-Details.png',
     "LeetCode": 'https://leetcode.com/static/images/LeetCode_Sharing.png',
